fba838bb9642180e58766542592051d2f260dfdf
Serena Nakatani-Brown <serena_1994@hotmail.co.uk>
2018-09-03 21:12:37 +0000

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,13 @@
 def square_array(array)
-  # your code here
+  newArray = []
+  array.each do |numbers|
+    numbers=numbers**2
+    newArray << numbers
+  end
+  newArray
 end
+
+numbers = [1, 2, 3]
+
+square_array(numbers)
+
